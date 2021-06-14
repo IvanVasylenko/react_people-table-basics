01618d80c17904c1341cd0d2d76ca93b7ec3eb9a
”Ivan <ivasylenko@ukr.net>
2021-06-14 15:25:59 +0300

    Updates

---
+++ precache-manifest.5d12643c13dc368f8ec5da5dffd0d41c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebe9667b61e12783badc41d60afd401",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e0b43e6e902a2ac6089",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "6d89489b9ae7bc617d2f",
+    "url": "./static/js/2.0522a680.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.0522a680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e0b43e6e902a2ac6089",
+    "url": "./static/js/main.803546e0.chunk.js"
+  },
+  {
+    "revision": "c7ae2510e9f54903a2ed",
+    "url": "./static/js/runtime-main.b51b247a.js"
+  }
+]);
